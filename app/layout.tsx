3c4d7572f36aff5f9b7a3a55969bb2149f315c4d
Vi <vix.b99@gmail.com>
2025-10-19 22:12:20 +0700

    config metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,34 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'Vĩ & Hiền Wedding',
-  description: 'A love story forever 💖',
-}
+// app/layout.tsx
+export const metadata = {
+  title: "Trang cưới Chấn Vĩ & Thị Hiền 💍",
+  description: "Trang cưới của chúng tôi, lưu lại những khoảnh khắc hạnh phúc nhất 💖",
+  openGraph: {
+    title: "Chấn Vĩ & Thị Hiền 💍",
+    description: "Trang cưới của chúng tôi, lưu lại những khoảnh khắc hạnh phúc nhất 💖",
+    url: "https://vixhien.vercel.app",
+    siteName: "Chấn Vĩ & Thị Hiền Wedding",
+    images: [
+      {
+        url: "https://vixhien.vercel.app/images/image.png",
+        width: 1200,
+        height: 630,
+        alt: "Ảnh cưới Chấn Vĩ & Thị Hiền 💍",
+      },
+    ],
+    locale: "vi_VN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Chấn Vĩ & Thị Hiền 💍",
+    description: "Trang cưới của chúng tôi 💖",
+    images: ["https://yourwedding.vercel.app/images/image.png"],
+  },
+};
+
 
 export default function RootLayout({
   children,
